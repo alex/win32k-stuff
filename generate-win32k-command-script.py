97f8ffe359b683d38739a75f28184f206b845439
Alex Gaynor <agaynor@mozilla.com>
2017-08-30 10:35:20 -0400

    WIN32U is sufficient on Windows 10 as far as I can tell

--- generate-win32k-command-script.py
+++ generate-win32k-command-script.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import click
 
 
-BREAKPOINT_LINE = """bp {module}!{func} ".echo '===WIN32K-START===';k;.echo '===WIN32K-END===';g"\r\n"""
+BREAKPOINT_LINE = """bp WIN32U!{func} ".echo '===WIN32K-START===';k;.echo '===WIN32K-END===';g"\r\n"""
 EXCLUDED_FUNCS = {
     # These are all called from the core event loop, and are thus incredibly
     # high volume, making the browser basically unusable if they're logged.
@@ -19,11 +19,7 @@ def main(input, output):
         func = line.strip()
         if func in EXCLUDED_FUNCS:
             continue
-        # Write all of these out, since I can't figure out which module things
-        # are in. windbg doesn't hard fail on non-existant things, so It's Fine
-        # (tm)
-        for module in ["USER32", "GDI32", "WIN32U"]:
-            output.write(BREAKPOINT_LINE.format(module=module, func=func))
+        output.write(BREAKPOINT_LINE.format(func=func))
 
 
 if __name__ == "__main__":
